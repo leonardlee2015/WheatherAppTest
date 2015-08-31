31d4770044f4f9e99d6175b44f7e0b1e151477b7
leonardlee2015 <leonardlee2013@gmail.com>
2015-08-31 16:11:34 +0800

    Initial Commit

---
+++ WheatherAppTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WheatherAppTest
+//
+//  Created by 李南 on 15/8/31.
+//  Copyright (c) 2015年 ctd.leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WheatherAppTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WheatherAppTest
+//
+//  Created by 李南 on 15/8/31.
+//  Copyright (c) 2015年 ctd.leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
